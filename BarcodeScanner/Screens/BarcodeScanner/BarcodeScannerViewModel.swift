8f87584f77e2fd5844f8781afb2a30580f362ba1
AndressRangell <andres.rangel555@gmail.com>
2024-05-20 18:57:06 -0500

    setup project

---
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by Andres Fabian Rangel Marino on 16/04/24.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+    
+}
